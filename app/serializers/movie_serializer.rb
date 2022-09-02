685368116c74c22ac57ab0a48189c1f26af7a620
youbaidjis <youba.etud@gmail.com>
2022-09-02 13:18:30 -0700

    Completed assignment

---
+++ app/serializers/movie_serializer.rb
@@ -0,0 +1,4 @@
+class MovieSerializer < ActiveModel::Serializer
+  attributes :id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director, :summary
+
+end
